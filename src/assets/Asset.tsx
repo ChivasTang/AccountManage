49ee0f687ec7c7641f436fc252f16696821cf57f
湯志華 <zhihua.tang.ra@ps.hitachi-solutions.com>
2025-03-29 03:09:31 +0900

    20250328

--- src/assets/Asset.tsx
+++ src/assets/Asset.tsx
@@ -1,7 +1,6 @@
 enum Asset {
-  icon_add = require('./icon_add.png'),
-  icon_back = require('./icon_back.png'),
   icon_close = require('./icon_close.png'),
+  icon_back = require('./icon_back.png'),
+  icon_add = require('./icon_add.png'),
 }
-
 export default Asset;
